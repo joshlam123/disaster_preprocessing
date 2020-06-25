28937f52a23315ef44fd2c38c6c6304e0a9fb735
Ying Sheng <yingshenglam@gmail.com>
2020-06-25 13:16:06 +0800

    modifiying bash script

--- example_1/generated/exec_gdal.sh
+++ example_1/generated/exec_gdal.sh
@@ -3,6 +3,13 @@ do
     NAME=$(echo $d | cut -d'.' -f 2 | cut -d'/' -f 3)
     echo "Converting $NAME using GDAL"
     gdal_translate -ot Float32 -of PCRaster -mo PCRASTER_VALUESCALE=VS_SCALAR $d "${NAME}.map"
+    echo "MOVING ${NAME}.map to folder /maps"
+    mv *.map* ./maps
+
 done
 
+parent=$(dirname $PWD)
+grandparent="$(dirname $parent)/code"
+# echo "$(find $grandparent -name "*.py")"
+python3 "$(grandparent)/generatemaps.py"
 
